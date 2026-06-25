533d5e6aa3cc1b6c362aae78325e32d9809c7c11
TenTen <kty2k15@gmail.com>
2026-06-25 18:33:16 +0900

    chore: inital project setup

---
+++ src/main/java/com/uniconvert/backend/UniconvertBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uniconvert.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniconvertBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniconvertBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniconvert/backend/UniconvertBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniconvert.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniconvertBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
