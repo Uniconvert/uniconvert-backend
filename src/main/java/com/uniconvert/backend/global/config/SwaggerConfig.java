9d5b9d4cdcc2a976e34ec335189ce85c50406e37
신은주 <sineunju598@gmail.com>
2026-07-19 00:47:52 +0900

    [ADD] swagger 기초 셋업

--- src/main/java/com/uniconvert/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/uniconvert/backend/global/config/SwaggerConfig.java
@@ -4,14 +4,19 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String BROWSER_LANGUAGE_HEADER = "BrowserLanguageHeader";
 
     @Bean
     public OpenAPI openAPI() {
@@ -25,13 +30,51 @@ public class SwaggerConfig {
                         .description("Uniconvert backend API documentation")
                         .contact(new Contact().name("Uniconvert")))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .components(new Components().addSecuritySchemes(
-                        securitySchemeName,
-                        new SecurityScheme()
-                                .name(securitySchemeName)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                ));
+                .components(new Components()
+                        .addSecuritySchemes(
+                                securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                        .addParameters(
+                                BROWSER_LANGUAGE_HEADER,
+                                new Parameter()
+                                        .name("X-Browser-Language")
+                                        .in("header")
+                                        .required(false)
+                                        .description("""
+                                                사용자의 브라우저 언어 정보입니다.
+
+                                                프론트엔드는 브라우저의 navigator.language 값을 전달합니다.
+                                                값이 없거나 지원하지 않는 값인 경우 서버는 ko-KR을 기본값으로 사용합니다.
+                                                """)
+                                        .schema(new Schema<String>()
+                                                .type("string")
+                                                .example("ko-KR")
+                                                ._default("ko-KR"))
+                        ));
+    }
+
+    /**
+     * 모든 API 경로에 X-Browser-Language 공통 헤더를 자동으로 노출한다.
+     * 컨트롤러마다 @Parameter 어노테이션을 반복 작성하지 않아도 되도록 전역 적용한다.
+     */
+    @Bean
+    public GlobalOpenApiCustomizer browserLanguageHeaderCustomizer() {
+        return openApi -> {
+            if (openApi.getPaths() == null) {
+                return;
+            }
+            openApi.getPaths().values().forEach(pathItem ->
+                    pathItem.readOperations().forEach(operation ->
+                            operation.addParametersItem(
+                                    new Parameter().$ref("#/components/parameters/" + BROWSER_LANGUAGE_HEADER)
+                            )
+                    )
+            );
+        };
     }
 }
